ccf746adf4797b691438255e5a9dd2860a593379
27Anurag <56363215+27Anurag@users.noreply.github.com>
2020-12-14 00:40:31 +0530

    for running stream

--- index.js
+++ index.js
@@ -8,47 +8,62 @@ function initMap(){
     //Map object.
     var map = new google.maps.Map(document.getElementById('map'), options);
 
-    //Bus locations and info.
-    var bus = [
-        {
-            location: {lat: 19.2856, lng: 72.8691} //Mira Road
-        },
-        {
-            location: {lat: 18.9912, lng: 72.8145}, //South Bombay
-            info: '<h3>Bus 1 reached at 12:05pm</h3>'
-        }
-    ];
+    //************************************************************************
+    var coord = [[90,90],[45,45],[180,180],[60,60]];
+    var counter=0; 
+    //coord keeps updating from database
+    var delay = 5000; //marker updates every 5 seconds
+    
+    var temp = 1000000; //for testing
+    var temp_ctr = 0; 
+    while(true){
+        //flag = fetch user input (if true break out of loop else continue)
+        temp_ctr += 0.1; if(temp_ctr==temp)break; //varna infi loop (for testing only)
 
-    // Loop through the bus locations.
-    for(var i = 0;i < bus.length;i++){
-        // Add marker
-        addMarker(bus[i]);
-      }
+        setTimeout(() => { counter++ ; }, delay); 
 
-    //Function to add markers dynamically.
-    function addMarker(buses){
-        var marker = new google.maps.Marker({
-            position: buses.location,
-            map:map,
-        });
 
-        //Check whether icon has been customized.
-        if(buses.iconImage){
-            marker.setIcon(buses.iconImage);
-        }
-  
-        //Check whether extra info is given.
-        if(buses.info){
-            var infoWindow = new google.maps.InfoWindow({
-                content: buses.info
+        //Bus locations and info.
+        var bus = [
+            {
+                location: {lat:coord[counter][0] , lng: coord[counter][1]} 
+            }
+           
+        ];
+
+        // Loop through the bus locations.
+        // for(var i = 0;i < bus.length;i++){
+        //     // Add marker
+        //     addMarker(bus[i]);
+        //   }
+
+        //Function to add markers dynamically.
+        function addMarker(buses){
+            var marker = new google.maps.Marker({
+                position: buses.location,
+                map:map,
             });
 
-            marker.addListener('click', function(){
-                infoWindow.open(map, marker);
-            });
-        }
-    }
+            //Check whether icon has been customized.
+            if(buses.iconImage){
+                marker.setIcon(buses.iconImage);
+            }
+      
+            //Check whether extra info is given.
+            if(buses.info){
+                var infoWindow = new google.maps.InfoWindow({
+                    content: buses.info
+                });
 
+                marker.addListener('click', function(){
+                    infoWindow.open(map, marker);
+                });
+            }
+        }
+
+    }//while loop
+    //************************************************************************
+    
     /*
     var marker = new google.maps.Marker({
       position:{lat:19.0760,lng:72.8777},
