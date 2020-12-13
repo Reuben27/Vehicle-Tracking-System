b00af22236108530cc0f8d0bbc98c5a053ee225b
27Anurag <56363215+27Anurag@users.noreply.github.com>
2020-12-14 01:15:54 +0530

    bus

--- index.js
+++ index.js
@@ -5,40 +5,10 @@ function initMap(){
       center: {lat:19.0760,lng:72.8777} //Coordinates of Mumbai
     }
     
-    //Map object.
-    var map = new google.maps.Map(document.getElementById('map'), options);
 
-    //************************************************************************
-    var buses = [[90,90],[45,45],[180,180],[60,60]];
-    var counter=0; 
-    //coord keeps updating from database
-    var delay = 5000; //marker updates every 5 seconds
-    
-    var temp = 1000000; //for testing
-    var temp_ctr = 0; 
-    while(true){
-        //flag = fetch user input (if true break out of loop else continue)
-        temp_ctr += 0.1; if(temp_ctr==temp)break; //varna infi loop (for testing only)
+     //Function to add markers dynamically.
 
-        setTimeout(() => { counter++ ; }, delay); 
-
-
-        //Bus locations and info.
-        var bus = [
-            {
-                location: {lat:buses[counter][0] , lng: buses[counter][1]} 
-            }
-           
-        ];
-        addMarker(buses[counter]);
-        // Loop through the bus locations.
-        // for(var i = 0;i < bus.length;i++){
-        //     // Add marker
-        //     addMarker(bus[i]);
-        //   }
-
-        //Function to add markers dynamically.
-        function addMarker(buses){
+     function addMarker(buses){
             var marker = new google.maps.Marker({
                 position: buses.location,
                 map:map,
@@ -60,7 +30,34 @@ function initMap(){
                 });
             }
         }
+    
+    //Map object.
+    var map = new google.maps.Map(document.getElementById('map'), options);
 
+    var buses = [[90,90],[45,45],[180,180],[60,60]];
+    var counter=0; 
+    //coord keeps updating from database
+    var delay = 5000; //marker updates every 5 seconds
+    
+    var temp = 1000000; //for testing
+    var temp_ctr = 0; 
+    while(true){
+        //flag = fetch user input (if true break out of loop else continue)
+        temp_ctr += 0.1; if(temp_ctr==temp)break; //varna infi loop (for testing only)
+
+        //Bus locations and info.
+        var bus = [
+            {
+                location: {lat:buses[counter][0] , lng: buses[counter][1]} 
+            }
+           
+        ];
+        
+        addMarker(bus[counter]);
+
+        setTimeout(() => { counter++ ; }, delay); 
+
+    
     }//while loop
     //************************************************************************
 
