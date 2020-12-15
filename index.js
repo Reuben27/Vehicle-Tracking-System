41d2f37da5f9af06f04270cb5efd11e1fa4dfab0
27Anurag <56363215+27Anurag@users.noreply.github.com>
2020-12-15 21:30:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,31 +34,60 @@ function initMap(){
     //Map object.
     var map = new google.maps.Map(document.getElementById('map'), options);
 
-    var buses = [[90,90],[45,45],[180,180],[60,60]];
+    var buses = [[19.0760,72.8777],[25.0760,80.8777],[30.0760,85.8777],[34.0760,90.8777]];
     var counter=0; 
     //coord keeps updating from database
-    var delay = 5000; //marker updates every 5 seconds
+    var delay = 15000; //marker updates every 5 seconds
     
     var temp = 1000000; //for testing
     var temp_ctr = 0; 
-    while(true){
-        //flag = fetch user input (if true break out of loop else continue)
-        temp_ctr += 0.1; if(temp_ctr==temp)break; //varna infi loop (for testing only)
 
+    var tt=0;
+    // while(true){
+        //flag = fetch user input (if true break out of loop else continue)
+        // temp_ctr += 1; if(temp_ctr==temp)break; //varna infi loop (for testing only)
+        // tt++; 
+        // if(tt==1000000){
+        //     addMarker(bus[counter]);
+        //     counter++; 
+        //     tt=0;
+        // }
         //Bus locations and info.
         var bus = [
             {
                 location: {lat:buses[counter][0] , lng: buses[counter][1]} 
-            }
+            },
            
         ];
-        
-        addMarker(bus[counter]);
 
-        setTimeout(() => { counter++ ; }, delay); 
+        var i = 1;                  //  set your counter to 1
+
+        function myLoop() {         //  create a loop function
+          setTimeout(function() {   //  call a 3s setTimeout when the loop is called
+            addMarker(bus[counter])
+            counter=counter+1;   //  your code here
+            i++;                    //  increment the counter
+            if (i < 10) {           //  if the counter < 10, call the loop function
+              myLoop();             //  ..  again which will trigger another 
+            }                       //  ..  setTimeout()
+          }, 5000)
+        }
+
+        addMarker(bus[counter])
+        counter++; 
+        addMarker(bus[counter])
+        counter++; 
+
+        myLoop(); 
+  
+
+        
+        // addMarker(bus[counter]);
+
+        // setTimeout(() => { counter++ ; }, delay); 
 
     
-    }//while loop
+    // }//while loop
     //************************************************************************
 
     /*
