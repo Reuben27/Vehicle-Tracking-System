435a022e6c5779946be854845d8f8a27715cfd45
Reuben27 <reuben.sd@iitgn.ac.in>
2020-12-18 20:16:53 +0530

    Updates

--- prac.js
+++ prac.js
@@ -6,23 +6,55 @@ function initMap() {
   const mumbai = { lat:19.0760,lng:72.8777 };
   const miraroad = {lat: 19.2856, lng: 72.8691};
   const southbombay = {lat: 18.9912, lng: 72.8145};
-  
+  const navimumbai = {lat: 19.0330, lng: 73.0297};
+
+  const input_variables = [[19.2856, 72.8691], [19.0330, 73.0297], [19.1856, 72.8592]];
+
   map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 12,
+    zoom: 8,
     center: mumbai,
     mapTypeId: "terrain",
   });
 
+  var curr = {lat: input_variables[0][0], lng: input_variables[0][1]};
+  addMarker(curr);
+
+  setTimeout(function(){
+    curr = {lat: input_variables[1][0], lng: input_variables[1][1]};
+    addMarker(curr);
+    deletemarker(0);
+  }, 10000);
+  
+  setTimeout(function(){
+    curr = {lat: input_variables[2][0], lng: input_variables[2][1]};
+    addMarker(curr);
+    deletemarker(1);
+  }, 20000);
+  
+  /*for(var i = 1; i < 3; i++){
+      delay(i, input_variables);
+  }*/
+  /*curr = {lat: input_variables[1][0], lng: input_variables[1][1]};
+  addMarker(curr);
+  curr = {lat: input_variables[2][0], lng: input_variables[2][1]};
+  addMarker(curr);
+  deletemarker(2);
+  */
+  /*for(var i = 0; i < 3; i++){
+    curr = {lat: input_variables[i][0], lng: input_variables[i][1]};
+    addMarker(curr);
+  }*/
+
   // This event listener will call addMarker() when the map is clicked.
-  map.addListener("click", (event) => {
+  /*map.addListener("click", (event) => {
     addMarker(event.latLng);
-  });
+  });*/
 
   // Adds a marker at the center of the map.
-  addMarker(miraroad);
+  /*addMarker(miraroad);
   setTimeout(function() { 
     addMarker(southbombay);
-    deletemarker(0);}, 10000);
+    deletemarker(0);}, 10000);*/
   //setTimeout(function() { deletemarker(0);}, 5000);
 }
 
@@ -37,6 +69,13 @@ function addMarker(location) {
 
 function deletemarker(i){
     markers[i].setMap(null);
+}
+
+function delay(i, input_variables){
+    setTimeout(function() { 
+        var cur = {lat: input_variables[i][0], lng: input_variables[i][1]};
+        addMarker(cur);
+        deletemarker(i-1);}, 10000);
 }
 
 // Sets the map on all markers in the array.
