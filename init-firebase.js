fbf9fc5968a5fc430193b28ae05fbe42e9ce7c4b
Reuben27 <reuben.sd@iitgn.ac.in>
2020-12-28 22:42:29 +0530

    Major Updates II

--- init-firebase.js
+++ init-firebase.js
@@ -1,21 +1,26 @@
-// TODO: Replace the following with your app's Firebase project configuration
-var firebaseConfig = {
-    /*
-    apiKey: "apiKey",
-    authDomain: "projectId.firebaseapp.com",
-    databaseURL: "https://databaseName.firebaseio.com",
-    storageBucket: "bucket.appspot.com"
-    */
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBGY4NS2f7IQoj6DTJZmF2DdA_hmuh5dLw",
+    authDomain: "poiuyt123.firebaseapp.com",
+    databaseURL: "https://poiuyt123-default-rtdb.firebaseio.com",
+    projectId: "poiuyt123",
+    storageBucket: "poiuyt123.appspot.com",
+    messagingSenderId: "1077239669792",
+    appId: "1:1077239669792:web:5c9a6c66dd730329204845"
   };
-  
   // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 
 //For reading data
-var ref = firebase.database().ref();
+//var ref = firebase.database().ref("");
 
+firebase.database().ref('poiuyt123-default-rtdb').on('value',(snap)=>{
+    console.log(snap.val());
+});
+/*
 ref.on("value", function(snapshot) {
    console.log(snapshot.val());
 }, function (error) {
    console.log("Error: " + error.code);
 });
+*/
---
+++ mappy.js
@@ -0,0 +1,67 @@
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+
+        anHttpRequest.open( "GET", aUrl, true );            
+        anHttpRequest.send( null );
+    }
+}
+
+let map;
+let markers = [];
+
+//To add markers and remove one by one.
+function initMap() {
+  const mumbai = { lat:19.0760,lng:72.8777 };
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 8,
+    center: mumbai,
+    mapTypeId: "terrain",
+  });
+
+  
+    var client = new HttpClient();
+    var obj; 
+    var lati;
+    var longi;
+    var time;
+
+    client.get('https://api.thingspeak.com/channels/1271022/feeds.json?results=2', function(response) {
+        console.log(response);
+        console.log("Hemlo");
+        obj = JSON.parse(response);
+        console.log(obj["feeds"]);
+        console.log(obj.feeds[1].field1);
+
+        lati = parseFloat(obj.feeds[1].field1);
+        console.log(typeof(lati));
+        console.log(lati);
+
+        longi = parseFloat(obj.feeds[1].field2);
+        console.log(longi);
+
+        time = obj.feeds[1].created_at;
+        console.log("Location Last Updated in");
+        console.log(time);
+    });
+
+    setTimeout(function(){
+    console.log(typeof(lati));
+    console.log(lati);
+    var curr = {lat: lati, lng: longi };
+    addMarker(curr);
+    }, 10000);
+    
+}
+
+  function addMarker(location) {
+    const marker = new google.maps.Marker({
+      position: location,
+      map: map,
+    });
+    markers.push(marker);
+  }
