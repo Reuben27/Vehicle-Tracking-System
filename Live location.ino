28a17c81acaaaad68422a9312d11b427218e3fe0
Rishabhji24 <68351949+Rishabhji24@users.noreply.github.com>
2020-12-30 02:18:31 +0530

    Update Live location.ino

--- Live location.ino
+++ Live location.ino
@@ -20,12 +20,12 @@ float longitude;
 void setup()
 {
   gprsSerial.begin(9600);               // the GPRS baud rate   
-  Serial.begin(9600);    // the GPRS baud rate 
+//  Serial.begin(9600);    // the GPRS baud rate 
   GPS.begin(GPSBaud);
-  latitude = gps.location.lat();
-  longitude = gps.location.lng();
-  Serial.println("Starting....");
-  Serial.println("Wait for initializing.");
+//  latitude = gps.location.lat();
+//  longitude = gps.location.lng();
+//  Serial.println("Starting....");
+//  Serial.println("Wait for initializing.");
   delay(30000);
 }
  
@@ -35,90 +35,90 @@ void loop()
   longitude = gps.location.lng();
   delay(100);   
      
-  Serial.print("Longitude = ");
-  Serial.print(longitude);
-  Serial.println(".");
-  Serial.print("Latitude = ");
-  Serial.print(latitude);
-  Serial.println(".");    
+//  Serial.print("Longitude = ");
+//  Serial.print(longitude);
+//  Serial.println(".");
+//  Serial.print("Latitude = ");
+//  Serial.print(latitude);
+//  Serial.println(".");    
   
 
-  if (gprsSerial.available())
-    Serial.write(gprsSerial.read());
+//  if (gprsSerial.available())
+//    Serial.write(gprsSerial.read());
  
   gprsSerial.println("AT");
-  delay(1000);
+  delay(500);
  
   gprsSerial.println("AT+CPIN?");
-  delay(1000);
+  delay(500);
  
   gprsSerial.println("AT+CREG?");
-  delay(1000);
+  delay(500);
  
   gprsSerial.println("AT+CGATT?");
-  delay(1000);
+  delay(500);
  
   gprsSerial.println("AT+CIPSHUT");
-  delay(1000);
+  delay(500);
  
   gprsSerial.println("AT+CIPSTATUS");
-  delay(2000);
+  delay(500);
  
   gprsSerial.println("AT+CIPMUX=0");
-  delay(2000);
+  delay(500);
  
-  ShowSerialData();
+//  ShowSerialData();
  
   gprsSerial.println("AT+CSTT=\"airtelgprs.com\"");//start task and setting the APN,
   delay(1000);
  
-  ShowSerialData();
+//  ShowSerialData();
  
   gprsSerial.println("AT+CIICR");//bring up wireless connection
-  delay(3000);
+  delay(1000);
  
-  ShowSerialData();
+//  ShowSerialData();
  
   gprsSerial.println("AT+CIFSR");//get local IP adress
-  delay(2000);
+  delay(1000);
  
-  ShowSerialData();
+//  ShowSerialData();
  
   gprsSerial.println("AT+CIPSPRT=0");
-  delay(3000);
+  delay(2000);
  
-  ShowSerialData();
+//  ShowSerialData();
   
   gprsSerial.println("AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",\"80\"");//start up the connection
-  delay(6000);
+  delay(4000);
  
-  ShowSerialData();
+//  ShowSerialData();
  
   gprsSerial.println("AT+CIPSEND");//begin send data to remote server
   delay(4000);
-  ShowSerialData();
+//  ShowSerialData();
   
   String str="GET https://api.thingspeak.com/update?api_key=AGSGWOJIMMP0HHQJ&field1=" + String(latitude) +"&field2="+String(longitude);
   Serial.println(str);
   gprsSerial.println(str);//begin send data to remote server
   
-  delay(4000);
-  ShowSerialData();
+  delay(2000);
+//  ShowSerialData();
  
   gprsSerial.println((char)26);//sending
-  delay(5000);//waitting for reply, important! the time is base on the condition of internet 
+  delay(1000);//waitting for reply, important! the time is base on the condition of internet 
   gprsSerial.println();
  
-  ShowSerialData();
+//  ShowSerialData();
  
-  gprsSerial.println("AT+CIPSHUT");//close the connection
-  delay(100);
-  ShowSerialData();
+//  gprsSerial.println("AT+CIPSHUT");//close the connection
+//  delay(1000);
+//  ShowSerialData();
 } 
-void ShowSerialData()
-{
-  while(gprsSerial.available()!=0)
-  Serial.write(gprsSerial.read());
-  delay(5000); 
-  
-}
+//void ShowSerialData()
+//{
+//  while(gprsSerial.available()!=0)
+//  Serial.write(gprsSerial.read());
+////  delay(5000); 
+//  
+//}
