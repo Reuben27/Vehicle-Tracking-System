3b98973b0443d0b14bbe455d777c3053ac0f148c
Reuben27 <reuben.sd@iitgn.ac.in>
2020-12-30 13:43:47 +0530

    Updates

--- mappy.js
+++ mappy.js
@@ -16,13 +16,13 @@ let markers = [];
 
 //To add markers and remove one by one.
 function initMap() {
-  const mumbai = { lat:19.0760,lng:72.8777 };
- 
+    const mumbai = { lat:19.0760,lng:72.8777 }; 
     var client = new HttpClient();
     var obj; 
     var lati;
     var longi;
     var time;
+    var prev;
 
     client.get('https://api.thingspeak.com/channels/1271022/feeds.json?results=2', function(response) {
         console.log(response);
@@ -33,10 +33,10 @@ function initMap() {
 
         lati = parseFloat(obj.feeds[1].field1);
         console.log(typeof(lati));
-        console.log(lati);
+        console.log("Latitude = " + lati);
 
         longi = parseFloat(obj.feeds[1].field2);
-        console.log(longi);
+        console.log("Longitude = " + longi);
 
         time = obj.feeds[1].created_at;
         console.log("Location Last Updated in");
@@ -44,23 +44,60 @@ function initMap() {
     });
 
     setTimeout(function(){
-    console.log(typeof(lati));
-    console.log(lati);
-    var curr = {lat: lati, lng: longi };
-    map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 16,
-        center: curr,
-        mapTypeId: "terrain",
-      }); 
-    addMarker(curr);
+        console.log(typeof(lati));
+        console.log(lati);
+        var curr = {lat: lati, lng: longi };
+        prev = curr;
+
+        map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 16,
+            center: curr,
+            mapTypeId: "terrain",
+        }); 
+        
+        addMarker(curr);
     }, 5000);
+
+    console.log("Hey everybody what you gonna do today!!");
+
+    setInterval(function(){
+        client.get('https://api.thingspeak.com/channels/1271022/feeds.json?results=2', function(response) {
+            console.log(response);
+            console.log("Hemlo");
+            obj = JSON.parse(response);
+            console.log(obj["feeds"]);
+            console.log(obj.feeds[1].field1);
+
+            lati = parseFloat(obj.feeds[1].field1);
+            console.log(typeof(lati));
+            console.log("Latitude = " + lati);
+
+            longi = parseFloat(obj.feeds[1].field2);
+            console.log("Longitude = " + longi);
+
+            time = obj.feeds[1].created_at;
+            console.log("Location Last Updated in");
+            console.log(time);
+        });
+
+        setTimeout(function(){
+            console.log(typeof(lati));
+            console.log(lati);
+            curr = {lat: lati, lng: longi };
+            if (curr != prev){
+                addMarker(curr);
+                map.setCenter(curr);
+                prev = curr;
+            }
+        }, 5000);
+    }, 15000);
     
 }
 
-  function addMarker(location) {
+function addMarker(location) {
     const marker = new google.maps.Marker({
-      position: location,
-      map: map,
+        position: location,
+        map: map,
     });
     markers.push(marker);
-  }
+}
